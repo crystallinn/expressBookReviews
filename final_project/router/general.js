c402808832e2f8528082e69229c35296c1bf4a6a
crystallinn <crystal.orc@gmail.com>
2024-08-12 17:58:31 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,15 +17,19 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the isbn parameter from the request URL and send the corresponding book's details
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the author parameter from the request URL and send the corresponding book's details
+    const author = req.params.author;
+    // Check if the author matches any books
+    res.send(books[author]);
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
@@ -40,3 +44,4 @@ public_users.get('/review/:isbn',function (req, res) {
 });
 
 module.exports.general = public_users;
+
