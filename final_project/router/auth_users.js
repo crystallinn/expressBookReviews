b76a569aa46273097f8f0b8a32ecae4b0a243f31
crystallinn <crystal.orc@gmail.com>
2024-08-26 18:15:38 -0700

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -33,29 +33,6 @@ const authenticatedUser = (username, password) => {
     }
 }
     
-const app = express();
-
-app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
-app.use(express.json());
-
-// Middleware to authenticate requests to "/customer" endpoint
-app.use("/customer", function auth(req, res, next) {
-    // Check if user is logged in and has valid access token
-    if (req.session.authorization) {
-        let token = req.session.authorization['accessToken'];
-        // Verify JWT token
-        jwt.verify(token, "access", (err, user) => {
-            if (!err) {
-                req.user = user;
-                next(); // Proceed to the next middleware
-            } else {
-                return res.status(403).json({ message: "User not authenticated" });
-            }
-        });
-    } else {
-        return res.status(403).json({ message: "User not logged in" });
-    }
-});
 
 // Login endpoint
 app.post("/login", (req, res) => {
