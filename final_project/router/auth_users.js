0ba780c520af6b170a9024bbd243005ec0ce5af5
crystallinn <crystal.orc@gmail.com>
2024-08-31 01:39:08 -0400

    update

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -34,29 +34,6 @@ const authenticatedUser = (username, password) => {
 }
     
 
-// Login endpoint
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    // Check if username or password is missing
-    if (!username || !password) {
-        return res.status(404).json({ message: "Error logging in" });
-    }
-    // Authenticate user
-    if (authenticatedUser(username, password)) {
-        // Generate JWT access token
-        let accessToken = jwt.sign({
-            data: password
-        }, 'access', { expiresIn: 60 * 60 });
-        // Store access token and username in session
-        req.session.authorization = {
-            accessToken, username
-        }
-        return res.status(200).send("User successfully logged in");
-    } else {
-        return res.status(208).json({ message: "Invalid Login. Check username and password" });
-    }
-});
 
 // Check if a user with the given username already exists
 const doesExist = (username) => {
