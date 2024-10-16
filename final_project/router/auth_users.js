ba2314d50b865b9ca86c8b9f126d37a287a217d7
crystallinn <crystal.orc@gmail.com>
2024-10-16 13:23:35 -0700

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -50,6 +50,34 @@ const doesExist = (username) => {
 }
 
 
+const app = express();
+app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
+app.use(express.json());
+// Middleware to authenticate requests to "/friends" endpoint
+app.use("/friends", function auth(req, res, next) {
+    // Check if user is logged in and has valid access token
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+        // Verify JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
+});
+
+
+
+
+
+
+
 
 
 // Add a book review
